c55876cc290c609473c91be424aad78c4dab8252
Avula Sai Praneeth Yaswanth Reddy <praneethavula160905@gmail.com>
2026-01-24 15:04:07 +0530

    My first portfolio commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'spin-slow': 'spin 12s linear infinite',
+        'reverse-spin': 'reverse-spin 10s linear infinite',
+      },
+      keyframes: {
+        'reverse-spin': {
+          from: { transform: 'rotate(360deg)' },
+          to: { transform: 'rotate(0deg)' },
+        }
+      }
+    },
+  },
+  plugins: [],
+}
